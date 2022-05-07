69e313219f2941754a783af8b0c7e47df070e4b7
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-05-07 23:25:57 +0700

    add: context: goroutine leak

--- context_test.go
+++ context_test.go
@@ -3,6 +3,7 @@ package gocontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -20,4 +21,31 @@ func TestContextWithValue(t *testing.T) {
 	contextB := context.WithValue(contextA, "nama", "Hafiz Ramadhan")
 	fmt.Println(contextB)
 	fmt.Println(contextB.Value("nama"))
+}
+
+func CreateCounter() chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	destination := CreateCounter()
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
